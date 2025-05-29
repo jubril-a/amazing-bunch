5cd4c6939ce0b5904a47b1913122f9cc61470724
Jubril <abdulsalamjubr3@gmail.com>
2025-05-29 11:47:14 +0100

    update contact page

---
+++ app/contact/components/Form.jsx
@@ -0,0 +1,26 @@
+const Form = () => {
+  return (
+    <section className="bg-gray-100 px-4 py-25">
+        <h2 className="relative text-center text-4xl max-w-fit mx-auto font-semibold mb-16 after:w-[50%] after:h-1 after:bg-primary after:absolute after:bottom-[-16px] after:left-[25%]">Send us a Message</h2>
+        <form action="" className="max-w-2xl mx-auto">
+            <div className="grid sm:grid-cols-2 gap-4">
+                <label htmlFor="Name">
+                    <span className="block mb-4">Name</span>
+                    <input className="border border-secondary hover:border-[#ff73b9] focus:border-primary focus:outline-0 h-12 px-2 w-full rounded-md" name="Name" type="text" />
+                </label>
+                <label htmlFor="Phone">
+                    <span className="block mb-4">Phone Number</span>
+                    <input className="border border-secondary hover:border-[#ff73b9] focus:border-primary focus:outline-0 h-12 px-2 w-full rounded-md" name="Phone" type="phone" />
+                </label>
+            </div>
+            <label htmlFor="Message" className="block my-4">
+                <span className="block mb-4">Your Message</span>
+                <textarea className="border border-secondary hover:border-[#ff73b9] focus:border-primary focus:outline-0 h-50 px-2 w-full rounded-md" name="Message" id="Message"></textarea>
+            </label>
+            <input type="submit" value="Send Message" className="w-full bg-primary hover:bg-secondary cursor-pointer p-3 text-white font-medium rounded-md" />
+        </form>
+    </section>
+  )
+}
+
+export default Form
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,6 +1,20 @@
+import Header from "./components/Header"
+import ContactBox from "./components/ContactBox"
+import Form from "./components/Form"
+
 const page = () => {
   return (
-    <div>page</div>
+    <>
+      <Header />
+      <section className="bg-gray-900">
+        <div className="max-w-6xl mx-auto grid min-[860px]:grid-cols-3 gap-4 px-4 py-15">
+          <ContactBox image="mail" info="Email us" value="ikhagbodehelen@yahoo.com" />
+          <ContactBox image="telephone" info="Call us" value="(+234) 8025304967" />
+          <ContactBox image="location" info="Our location" value="JoyGood Mall, KM 27, Lekki-Epe Expressway" />
+        </div>
+      </section>
+      <Form />
+    </>
   )
 }
 
