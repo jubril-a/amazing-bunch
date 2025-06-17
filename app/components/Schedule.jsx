815304755df85a2c98c2444609561c13150349cc
Abdulsalam <abdulsalamjubr3@gmail.com>
2025-06-17 18:18:22 +0300

    add form handling

--- app/components/Schedule.jsx
+++ app/components/Schedule.jsx
@@ -11,7 +11,7 @@ const Schedule = () => {
           <div className="relative">
             <h2 className={`${Kugile.className} text-center text-5xl mb-3.5 text-white`}>Book Your Appointment</h2>
             <p className="text-center text-white mb-4.5">Say goodbye to long waits and hello to convenience. Schedule your next appointment with just a few clicks.</p>
-            <Link href="" className="flex items-center gap-2 bg-[#fff] py-4 px-7 rounded-full w-fit m-auto hover:bg-[#ff6ec3] group">
+            <Link href="https://wa.me/8025304967" className="flex items-center gap-2 bg-[#fff] py-4 px-7 rounded-full w-fit m-auto hover:bg-[#ff6ec3] group">
               <img className="group-hover:invert-100" src="/icons/whatsapp.png" alt="" width={32} height={32} />
               <span className="group-hover:text-white font-medium">Chat on WhatsApp</span>
             </Link>
--- app/contact/components/Form.jsx
+++ app/contact/components/Form.jsx
@@ -2,20 +2,20 @@ const Form = () => {
   return (
     <section className="bg-gray-100 px-4 py-25">
         <h2 className="relative text-center text-4xl max-w-fit mx-auto font-semibold mb-16 after:w-[50%] after:h-1 after:bg-primary after:absolute after:bottom-[-16px] after:left-[25%]">Send us a Message</h2>
-        <form action="" className="max-w-2xl mx-auto">
+        <form method="POST" action="https://formspree.io/f/mwpbqvdg" className="max-w-2xl mx-auto">
             <div className="grid sm:grid-cols-2 gap-4">
                 <label htmlFor="Name">
-                    <span className="block mb-4">Name</span>
-                    <input className="border border-secondary hover:border-[#ff73b9] focus:border-primary focus:outline-0 h-12 px-2 w-full rounded-md" name="Name" type="text" />
+                    <span className="block mb-2">Name</span>
+                    <input className="border border-secondary hover:border-[#ff73b9] focus:border-primary focus:bg-white focus:outline-0 h-12 px-2 w-full rounded-md" name="Name" type="text" />
                 </label>
                 <label htmlFor="Phone">
-                    <span className="block mb-4">Phone Number</span>
-                    <input className="border border-secondary hover:border-[#ff73b9] focus:border-primary focus:outline-0 h-12 px-2 w-full rounded-md" name="Phone" type="phone" />
+                    <span className="block mb-2">Phone Number</span>
+                    <input className="border border-secondary hover:border-[#ff73b9] focus:border-primary focus:bg-white focus:outline-0 h-12 px-2 w-full rounded-md" name="Phone" type="phone" />
                 </label>
             </div>
             <label htmlFor="Message" className="block my-4">
-                <span className="block mb-4">Your Message</span>
-                <textarea className="border border-secondary hover:border-[#ff73b9] focus:border-primary focus:outline-0 h-50 px-2 w-full rounded-md" name="Message" id="Message"></textarea>
+                <span className="block mb-2">Your Message</span>
+                <textarea className="border border-secondary hover:border-[#ff73b9] focus:border-primary focus:bg-white focus:outline-0 h-50 px-2 w-full rounded-md" name="Message" id="Message"></textarea>
             </label>
             <input type="submit" value="Send Message" className="w-full bg-primary hover:bg-secondary cursor-pointer p-3 text-white font-medium rounded-md" />
         </form>
